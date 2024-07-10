bb47d9cf5afeb09661162177509e8ca7ec537d8d
Ubuntu <ubuntu@ip-172-31-31-112.ec2.internal>
2024-07-10 02:19:52 +0000

    updated

--- app.js
+++ app.js
@@ -1,9 +1,27 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const os = require('os');
+const networkInterfaces = os.networkInterfaces();
+
+function getServerIP() {
+  for (const interfaceName in networkInterfaces) {
+    const networkInterface = networkInterfaces[interfaceName];
+    for (const networkAddress of networkInterface) {
+      if (networkAddress.family === 'IPv4' && !networkAddress.internal) {
+        return networkAddress.address;
+      }
+    }
+  }
+  return 'Unable to determine IP address';
+}
+
+app.set('trust proxy', true);
 
 app.get('/', (req, res) => {
-  res.send('Hello, World!');
+  const clientIp = req.ip; // Express will use X-Forwarded-For if trust proxy is enabled
+  
+  res.send(`Hello, World! Client IP is ${clientIp} and Server IP is ${getServerIP()}`);
 });
 
 app.listen(port, () => {
